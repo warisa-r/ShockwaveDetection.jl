3fa02770fc741e2d0cb4f1463a3d201e4dddd648
Warisa <compund555@gmail.com>
2024-06-21 23:00:05 +0200

    change some names

--- examples/1D_shock_scenario2_detection.jl
+++ examples/1D_shock_scenario2_detection.jl
@@ -7,4 +7,4 @@ detection_alg = GradientEntropyShockDetectionAlgo(DRY_AIR, 0.2)
 # Plot the shock create_wave_animation
 shock_positions_over_time = detect(flow_data, detection_alg)
 anim_with_shock = create_wave_animation_with_shock(flow_data, shock_positions_over_time)
-create_tube_field_evo_with_shock(flow_data, shock_positions_over_time, :velocity_field)
+#create_tube_field_evo_with_shock(flow_data, shock_positions_over_time, :velocity_field)
