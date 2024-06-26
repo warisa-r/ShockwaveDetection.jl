d06e2420bc3272d90223f4eb3dfd9edf1b2e32bd
Warisa <compund555@gmail.com>
2024-06-26 09:43:52 +0200

    why use atan / ... when u can use atan with 2 arguments. Why is the direction still wrong

--- examples/2D_SOD_shock_tube.jl
+++ examples/2D_SOD_shock_tube.jl
@@ -4,7 +4,7 @@ using ShockwaveProperties
 using Unitful
 using ShockwaveDetection
 
-flow_data = FlowData("examples/data/sod_shock_orb.tape", false)
+flow_data = FlowData("examples/data/sod_shock_right_2d.tape", false)
 
-shock_positions_over_time, angle_estimated = detect(flow_data, ImageProcessingShockDetectionAlgo(0.7, :sobel))
+shock_positions_over_time, angle_estimated = detect(flow_data, ImageProcessingShockDetectionAlgo(0.7, :prewitt))
 create_heatmap_evo_with_shock(flow_data, shock_positions_over_time, angle_estimated, :density_field, true)
