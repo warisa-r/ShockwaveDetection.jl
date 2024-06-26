bd1bbfc3af7887779b43069b8cc69c7ccbb4453b
Warisa <compund555@gmail.com>
2024-06-26 17:48:07 +0200

    update arctan to be of correct direction

--- examples/2D_SOD_shock_tube.jl
+++ examples/2D_SOD_shock_tube.jl
@@ -4,7 +4,7 @@ using ShockwaveProperties
 using Unitful
 using ShockwaveDetection
 
-flow_data = FlowData("examples/data/sod_shock_right_2d.tape", false)
+flow_data = FlowData("examples/data/sod_shock_orb.tape", false)
 
-shock_positions_over_time, angle_estimated = detect(flow_data, ImageProcessingShockDetectionAlgo(0.7, :prewitt))
+shock_positions_over_time, angle_estimated = detect(flow_data, ImageProcessingShockDetectionAlgo(0.5, :prewitt))
 create_heatmap_evo_with_shock(flow_data, shock_positions_over_time, angle_estimated, :density_field, true)
