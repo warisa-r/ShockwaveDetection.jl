bf6ef395d46285ab22d1fcf62831619874b971b6
Warisa <compund555@gmail.com>
2024-08-31 19:55:59 +0200

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,12 @@ using ShockwaveDetection
 @testset "Compiles" begin
     using ShockwaveDetection
 end
+
+@testset "1D shock detection" begin
+    include("../examples/sod_shock_tube_1d.jl")
+    rm("density_velocity_pressure_over_time_with_shock_positions.gif")
+end
+
+@testset "2D shock detection" begin
+    include("../examples/sod_shock_tube_2d.jl")
+end
