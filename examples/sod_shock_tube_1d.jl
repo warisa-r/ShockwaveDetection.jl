026e0c7b2ebf8cc5fa08148e3d53a016553c96f1
Warisa <compund555@gmail.com>
2024-08-31 20:02:50 +0200

    edit the directory so this can run properly in testing

--- examples/sod_shock_tube_1d.jl
+++ examples/sod_shock_tube_1d.jl
@@ -4,7 +4,7 @@ using ShockwaveProperties
 using Unitful
 using ShockwaveDetection
 
-flow_data = FlowData("examples/data/sod_shock_left_1d.tape", false)
+flow_data = FlowData("data/sod_shock_left_1d.tape", false)
 
 shock_positions_over_time = detect(flow_data, GradientShockDetectionAlgo(0.5))
 anim  = create_wave_animation_with_shock(flow_data, shock_positions_over_time)
