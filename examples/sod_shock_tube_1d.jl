b5449ab37d6508aab3950b704f1aa2728029742a
Warisa <compund555@gmail.com>
2024-08-31 20:21:47 +0200

    see where the data is being looked for

--- examples/sod_shock_tube_1d.jl
+++ examples/sod_shock_tube_1d.jl
@@ -7,6 +7,15 @@ using ShockwaveDetection
 # Print the current path of this file
 println("Current file path: ", @__FILE__)
 
+# Get the directory of the current script
+script_dir = @__DIR__
+
+# Construct the absolute path to the data file
+data_file_path = joinpath(script_dir, "data", "sod_shock_left_1d.tape")
+
+# Print the absolute path of the data file
+println("Data file path: ", data_file_path)
+
 flow_data = FlowData("data/sod_shock_left_1d.tape", false)
 
 shock_positions_over_time = detect(flow_data, GradientShockDetectionAlgo(0.5))
