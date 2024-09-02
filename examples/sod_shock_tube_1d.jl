8c6c6a1e9f78bd8926a94937c10c043cb3e6ab38
Ina <ina.dzhingarska2002@gmail.com>
2024-09-02 15:20:39 +0200

    pipeline 1D

--- examples/sod_shock_tube_1d.jl
+++ examples/sod_shock_tube_1d.jl
@@ -4,7 +4,12 @@ using ShockwaveProperties
 using Unitful
 using ShockwaveDetection
 
-flow_data = FlowData("example/data/sod_shock_left_1d.tape", false)
+flow_data = FlowData("examples/data/sod_shock_left_1d.tape", false)
+point_detect_algo = GradientShockDetectionAlgo(0.5)
 
-shock_positions_over_time = detect(flow_data, GradientShockDetectionAlgo(0.5))
-anim  = create_wave_animation_with_shock(flow_data, shock_positions_over_time)
+detection = detect(flow_data, point_detect_algo)
+
+#plot_shock_fits_over_time(flow_data, detection, true)
+
+#shock_positions_over_time = detect(flow_data, GradientShockDetectionAlgo(0.5))
+anim  = create_wave_animation_with_shock(flow_data, detection)
--- src/pipeline.jl
+++ src/pipeline.jl
@@ -17,3 +17,16 @@ function detect(flow_data::FlowData, shock_point_algo::Abstract2DShockDetectionA
     shock_fits_over_time = fit_shock_clusters_over_time(shock_clusters_over_time)
     return ShockDetectionResult2D(shock_positions_over_time, shock_clusters_over_time, shock_fits_over_time)
 end
+
+struct ShockDetectionResult1D
+    shock_positions_over_time::Vector{Any}
+end
+
+function detect(flow_data::FlowData, shock_point_algo::Abstract1DShockDetectionAlgo)
+    has_obstacle = false
+    if isnothing(flow_data.u)
+        has_obstacle = true
+    end
+    shock_positions_over_time = detect_shock_points(flow_data, shock_point_algo, has_obstacle)
+    return ShockDetectionResult1D(shock_positions_over_time)
+end
