1e7e6a372596c98a53a636d9ba6d8c551aa31cc2
Warisa <compund555@gmail.com>
2024-09-02 14:08:43 +0200

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,13 @@ using ShockwaveDetection
 @testset "Compiles" begin
     using ShockwaveDetection
 end
+
+@testset "obstacle shock detection" begin
+    flow_data = FlowData(joinpath(DATA_DIR, "circular_obstacle_radius_1.celltape"), false)
+    point_detect_algo = ImageProcessingShockDetectionAlgo(0.2, :prewitt)
+    dbscan_algo = DBSCANAlgo(0.25, 3, 10)
+    detection = detect(flow_data, point_detect_algo, dbscan_algo)
+    plot_shock_fits_over_time(flow_data, detection, false)
+    create_heatmap_evo_with_shock(flow_data, detection, :density_field, true, false)
+    rm("density_field_evolution.gif")
+end
