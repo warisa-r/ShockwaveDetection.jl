ff034f88ae1a56b3c763820fcdcd247589c51390
Elena Ivanova <94645016+elenaiwnw@users.noreply.github.com>
2024-09-05 14:09:30 +0200

    Update pipeline.jl

--- src/pipeline.jl
+++ src/pipeline.jl
@@ -7,9 +7,14 @@ struct ShockDetectionResult2D
 end
 
 # Pipeline is only for 2D shock detection now
-function detect(flow_data::FlowData, shock_point_algo::Abstract2DShockDetectionAlgo, cluster_algo::DBSCANAlgo)
-    shock_positions_over_time = detect_shock_points(flow_data, shock_point_algo)
+function detect(flow_data::FlowData, shock_point_algo::Abstract2DShockDetectionAlgo, cluster_algo::DBSCANAlgo, make_initial_guess::Bool)
+    has_obsticle = false
+    if isnothing(flow_data.u)
+        has_obsticle = true
+    end
+    
+    shock_positions_over_time = detect_shock_points(flow_data, shock_point_algo, has_obstacle)
     shock_clusters_over_time = cluster_shock_points(cluster_algo, shock_positions_over_time, flow_data)
-    shock_fits_over_time = fit_shock_clusters_over_time(shock_clusters_over_time)
+    shock_fits_over_time = fit_shock_clusters_over_time(shock_clusters_over_time, make_initial_guess)
     return ShockDetectionResult2D(shock_positions_over_time, shock_clusters_over_time, shock_fits_over_time)
 end
