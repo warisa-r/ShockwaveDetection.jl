30a5c3de76864f40333ba4ee71d1708a922b138d
Ina <ina.dzhingarska2002@gmail.com>
2024-09-05 14:50:57 +0200

    pipeline touchups

--- examples/sod_shock_tube_1d.jl
+++ examples/sod_shock_tube_1d.jl
@@ -6,9 +6,9 @@ using ShockwaveDetection
 using Distributions
 
 # Create a NoiseData instance
-noise_data = NoiseData(0.01, Normal(0, 1))  # 1% noise intensity, Gaussian distribution
+#noise_data = NoiseData(0.01, Normal(0, 1))  # 1% noise intensity, Gaussian distribution
 
-flow_data = FlowData("examples/data/sod_shock_left_1d.tape", false, noise_data)
+flow_data = FlowData("examples/data/sod_shock_left_1d.tape", false)
 
 point_detect_algo = GradientShockDetectionAlgo(0.5)
 
@@ -16,5 +16,5 @@ detection = detect(flow_data, point_detect_algo)
 
 #plot_shock_fits_over_time(flow_data, detection, true)
 
-#shock_positions_over_time = detect(flow_data, GradientShockDetectionAlgo(0.5))
+shock_positions_over_time = detect(flow_data, point_detect_algo)
 anim  = create_wave_animation_with_shock(flow_data, detection)
--- src/pipeline.jl
+++ src/pipeline.jl
@@ -1,32 +1,29 @@
-#TODO: pipeline 1D shock detection too
+abstract type AbstractShockDetectionResult end
 
-struct ShockDetectionResult2D
+struct ShockDetectionResult2D <: AbstractShockDetectionResult
     shock_positions_over_time::Vector{Any}
     shock_clusters_over_time::Vector{Any}
     shock_fits_over_time::Vector{Any}
 end
 
-# Pipeline is only for 2D shock detection now
-function detect(flow_data::FlowData, shock_point_algo::Abstract2DShockDetectionAlgo, cluster_algo::DBSCANAlgo)
-    has_obstacle = false
-    if isnothing(flow_data.u)
-        has_obstacle = true
-    end
-    shock_positions_over_time = detect_shock_points(flow_data, shock_point_algo, has_obstacle)
-    shock_clusters_over_time = cluster_shock_points(cluster_algo, shock_positions_over_time, flow_data)
-    shock_fits_over_time = fit_shock_clusters_over_time(shock_clusters_over_time)
-    return ShockDetectionResult2D(shock_positions_over_time, shock_clusters_over_time, shock_fits_over_time)
-end
-
-struct ShockDetectionResult1D
+struct ShockDetectionResult1D <: AbstractShockDetectionResult
     shock_positions_over_time::Vector{Any}
 end
 
-function detect(flow_data::FlowData, shock_point_algo::Abstract1DShockDetectionAlgo)
-    has_obstacle = false
-    if isnothing(flow_data.u)
-        has_obstacle = true
-    end
+# Pipeline for 2D shock detection
+function detect(flow_data::FlowData, shock_point_algo::Abstract2DShockDetectionAlgo, cluster_algo::DBSCANAlgo)
+    has_obstacle = isnothing(flow_data.u)  # Check if the flow data has an obstacle
     shock_positions_over_time = detect_shock_points(flow_data, shock_point_algo, has_obstacle)
+    shock_clusters_over_time = cluster_shock_points(cluster_algo, shock_positions_over_time, flow_data)
+    shock_fits_over_time = fit_shock_clusters_over_time(shock_clusters_over_time)
+    
+    return ShockDetectionResult2D(shock_positions_over_time, shock_clusters_over_time, shock_fits_over_time)
+end
+
+# Pipeline for 1D shock detection
+function detect(flow_data::FlowData, shock_point_algo::Abstract1DShockDetectionAlgo)
+    has_obstacle = isnothing(flow_data.u)  # Check if the flow data has an obstacle
+    shock_positions_over_time = detect_shock_points(flow_data, shock_point_algo, has_obstacle)
+    
     return ShockDetectionResult1D(shock_positions_over_time)
 end
